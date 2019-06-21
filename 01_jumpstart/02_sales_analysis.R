e861964eea83b32bf5232c66df90f25509950c4d
Jason Dexter <jddexter@ucdavis.edu>
2019-06-21 10:42:11 -0700

    added comment to wk 1 code

--- 01_jumpstart/02_sales_analysis.R
+++ 01_jumpstart/02_sales_analysis.R
@@ -25,6 +25,7 @@ bikeshops_tbl <- read_excel("00_data/bike_sales/data_raw/bikeshops.xlsx")
 
 orderlines_tbl <- read_excel("00_data/bike_sales/data_raw/orderlines.xlsx")
 
+
 # 3.0 Examining Data ----
 
 bikes_tbl
@@ -33,4 +34,23 @@ glimpse(bikes_tbl)
 
 bikeshops_tbl
 
-orderlines_tbl # connective tissue between bikes and bikeshops
+orderlines_tbl #* connective tissue between bikes and bikeshops
+
+
+# 4.0 Joining Data
+
+orderlines_tbl 
+bikes_tbl
+
+left_join(orderlines_tbl, bikes_tbl, by = c("product.id" = "bike.id"))
+
+bike_orderlines_joined_tbl <- orderlines_tbl %>% 
+    left_join(bikes_tbl, by = c("product.id" = "bike.id")) %>% 
+    left_join(bikeshops_tbl, by = c("customer.id" = "bikeshop.id"))
+
+bike_orderlines_joined_tbl
+
+bike_orderlines_joined_tbl %>% glimpse()
+
+# 5.0 Wrangling Data ----
+
