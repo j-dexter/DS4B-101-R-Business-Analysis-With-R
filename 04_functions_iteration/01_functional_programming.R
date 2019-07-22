6b7c2866a4f013fbe9d14f95e886713f40cb4a76
Jason Dexter <jddexter@ucdavis.edu>
2019-07-21 22:19:27 -0700

    completed wk5 funcitonal programming section

--- 04_functions_iteration/01_functional_programming.R
+++ 04_functions_iteration/01_functional_programming.R
@@ -257,24 +257,122 @@ bikes_tbl %>%
         disc      = model_tier %>% str_to_lower() %>% str_detect("disc") %>% as.numeric()
     )
 
+# Build function
+separate_bike_model <- function(data, keep_model_column = T, append = T) {
+    
+    if (!append) {
+        data <- data %>% select(model)
+    }
+    
+    # Pipeline
+    output_tbl <- data %>% 
+        
+        # Fix typo
+        mutate(model = case_when(
+            model == "CAAD Disc Ultegra" ~ "CAAD12 Disc Ultegra",
+            model == "Syapse Carbon Tiagra" ~ "Synapse Carbon Tiagra",
+            model == "Supersix Evo Hi-Mod Utegra" ~ "Supersix Evo Hi-Mod Ultegra",
+            TRUE ~ model
+        )) %>%
+        
+        # separate using spaces
+        separate(col     = model, 
+                 into    = str_c("model_", 1:7), 
+                 sep     = " ", 
+                 remove  = FALSE, 
+                 fill    = "right") %>%
+        
+        # creating a "base" feature
+        mutate(model_base = case_when(
+            
+            # Fix Supersix Evo
+            str_detect(str_to_lower(model_1), "supersix") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Fat CAAD bikes
+            str_detect(str_to_lower(model_1), "fat") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Beast of the East
+            str_detect(str_to_lower(model_1), "beast") ~ str_c(model_1, model_2, model_3, model_4, sep = " "),
+            
+            # Fix Bad Habit
+            str_detect(str_to_lower(model_1), "bad") ~ str_c(model_1, model_2, sep = " "),
+            
+            # Fix Scalpel 29
+            str_detect(str_to_lower(model_2), "29") ~ str_c(model_1, model_2, sep = " "),
+            
+            # catch all
+            TRUE ~ model_1)
+        ) %>%
+        
+        # Get "tier" feature
+        mutate(model_tier = model %>% str_replace(model_base, replacement = "") %>% str_trim()) %>%
+        
+        # Remove unnecessary columns
+        select(-matches("model_[0-9]")) %>%
+        
+        # Create Flags
+        mutate(
+            black     = model_tier %>% str_to_lower() %>% str_detect("black") %>% as.numeric(),
+            hi_mod    = model_tier %>% str_to_lower() %>% str_detect("hi-mod") %>% as.numeric(),
+            team      = model_tier %>% str_to_lower() %>% str_detect("team") %>% as.numeric(),
+            red       = model_tier %>% str_to_lower() %>% str_detect("red") %>% as.numeric(),
+            ultegra   = model_tier %>% str_to_lower() %>% str_detect("ultegra") %>% as.numeric(),
+            dura_ace  = model_tier %>% str_to_lower() %>% str_detect("dura ace") %>% as.numeric(),
+            disc      = model_tier %>% str_to_lower() %>% str_detect("disc") %>% as.numeric()
+        )
+    
+    if (!keep_model_column) output_tbl <- output_tbl %>% select(-model)
+    
+    return(output_tbl)
+    
+}
 
-
-
-
+#* test function
+bikes_tbl %>% 
+    separate_bike_model(keep_model_column = T, append = T)
 
 
 # 6.0 SAVING AND SOURCING FUNCTIONS ----
 
 # 6.1 Create folder and file ----
+fs::dir_create("00_scripts")
 
-
+path <- "00_scripts/separate_bikes_and_outlier_detection.R"
+fs::file_create(path)
 
 # 6.2 Build and add header ----
+file_header_text <- str_glue(
+"
+# SEPARATE BIKE MODELS AND DETECT OUTLIERS ----
 
+# separate_bike_models(): A tidy funciton to separate the model column into engineered feaures.
 
+# detect_outliers(): A vectorized functin that detects outliers using TRUE/FALSE output.
+
+# Libraries ----
+library(tidyverse)
+
+"
+)
+
+write_lines(file_header_text, path = path)
 
 # 6.3 Add functions with dump() ----
 
+c("separate_bike_model", "detect_outliers") %>% 
+    dump(file = "00_scripts/separate_bikes_and_outlier_detection.R",
+         append = TRUE)
 
 
 # 6.4 Source function ----
+
+rm("detect_outliers")
+rm("separate_bike_model")
+
+source("00_scripts/separate_bikes_and_outlier_detection.R")
+
+bikes_tbl %>% 
+    separate_bike_model()
+
+bikes_tbl %>% 
+    mutate(outliers = detect_outliers(x = price))
